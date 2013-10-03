f6766186182b69291ccf34a8be666354e03fbfa8
Amal Hussein <amalhussein08@gmail.com>
2013-10-03 07:04:51 -0400

    initial commit

--- main.rb
+++ main.rb
@@ -3,18 +3,17 @@ require 'pry'
 
 	#valid_ip =[]
 def valid_ip_addresses (valid_ip)
-		valid_ip.scan(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b\.?/)
+		valid_ip.scan(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
 end 
 	a = valid_ip_addresses(@source_text)
-
+	puts a
 
 
 def valid_mit_ip_addresses(mit_ip)
-	mit_ip.scan(/\b\d{2}\.\d{1,3}\.\d{1,3}\.\d{1,2}\b\?/)
+	mit_ip.scan(/\d{2}\.\d{3}\.\d{2}\.\d{1,3}/)
 end
 	b = valid_mit_ip_addresses(@source_text)
-
-
+	puts b 
 
 def non_mit_ip_addresses 
 end 
@@ -25,8 +24,6 @@ end
 
 c = valid_phone_numbers(@source_text)
 
-
-
 def area_codes 
 end
 
@@ -34,4 +31,8 @@ def email_addresses(email_add)
 	email_add.scan(/([a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z])/)
 end
 
-d =email_addresses(@source_text)
+d = email_addresses(@source_text).flatten
+
+puts d 
+
+binding.pry 
